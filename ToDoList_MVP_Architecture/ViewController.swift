838828f0f76888b565e4334d8f559492a1084068
Aglarov <shamil.aglarov@gmail.com>
2023-06-15 00:44:10 +0300

    Initial Commit

---
+++ ToDoList_MVP_Architecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList_MVP_Architecture
+//
+//  Created by Shamil Aglarov on 15.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
